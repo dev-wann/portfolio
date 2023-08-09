d56b09dc77770c5ec13d88186a72088d2a4c2346
dev-wann <swcho8220@gmail.com>
2023-08-09 21:25:25 +0900

    [chore] navigation 및 contact 코드 정리

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import styles from './navigation.module.css';
 import Link from 'next/link';
 
--- app/components/contact/ContactComponent.tsx
+++ app/components/contact/ContactComponent.tsx
@@ -2,71 +2,59 @@ import Image from 'next/image';
 import styles from './contact.module.css';
 import Link from 'next/link';
 export default function ContactComponent() {
-  const email = (
-    <div className={styles.icon}>
-      <Image
-        src="/img/contact/mail.svg"
-        alt="Email icon"
-        fill={true}
-        style={{ objectFit: 'contain', filter: 'invert(100%)' }}
-      ></Image>
-    </div>
-  );
-
-  const github = (
-    <div className={styles.icon}>
-      <Image
-        src="/img/github.svg"
-        alt="GitHub icon"
-        fill={true}
-        style={{ objectFit: 'contain' }}
-      ></Image>
-    </div>
-  );
-
-  const blog = (
-    <div className={styles.icon}>
-      <Image
-        src="/img/contact/article.svg"
-        alt="Blog icon"
-        fill={true}
-        style={{ objectFit: 'contain', filter: 'invert(100%)' }}
-      ></Image>
-    </div>
-  );
-
-  const footer = (
-    <div style={{ width: '100%', height: '10px' }} id="footer"></div>
-  );
-
   return (
     <>
-      <div className="wrapper" id="contact">
-        <div className={styles.contactWrapper} id="conatactWrapper">
-          <p className={styles.title}>Contact</p>
-          <div className={styles.contentWrapper}>
-            <div className={styles.content}>
-              {email}
-              <span className={styles.desc}>E-MAIL</span>
-              <span className={styles.desc}>swcho8220@gmail.com</span>
-              <Link href="mailto:swcho8220@gmail.com"></Link>
-            </div>
-            <div className={styles.content}>
-              {github}
-              <span className={styles.desc}>GitHub</span>
-              <span className={styles.desc}>https://github.com/dev-wann</span>
-              <Link href="https://github.com/dev-wann"></Link>
-            </div>
-            <div className={styles.content}>
-              {blog}
-              <span className={styles.desc}>Blog</span>
-              <span className={styles.desc}>https://dev-wann.tistory.com</span>
-              <Link href="https://dev-wann.tistory.com"></Link>
-            </div>
+      <div className={styles.contactWrapper} id="conatactWrapper">
+        <p className={styles.title}>Contact</p>
+        <div className={styles.contentWrapper}>
+          <div className={styles.content}>
+            <div className={styles.icon}>{images.email}</div>
+            <span className={styles.desc}>E-MAIL</span>
+            <span className={styles.desc}>swcho8220@gmail.com</span>
+            <Link href="mailto:swcho8220@gmail.com"></Link>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.icon}>{images.github}</div>
+            <span className={styles.desc}>GitHub</span>
+            <span className={styles.desc}>https://github.com/dev-wann</span>
+            <Link href="https://github.com/dev-wann"></Link>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.icon}>{images.blog}</div>
+            <span className={styles.desc}>Blog</span>
+            <span className={styles.desc}>https://dev-wann.tistory.com</span>
+            <Link href="https://dev-wann.tistory.com"></Link>
           </div>
         </div>
-        {footer}
       </div>
+      <div className={styles.footer} id="footer"></div>
     </>
   );
 }
+
+const images = {
+  email: (
+    <Image
+      src="/img/contact/mail.svg"
+      alt="Email icon"
+      fill={true}
+      style={{ objectFit: 'contain', filter: 'invert(100%)' }}
+    ></Image>
+  ),
+  github: (
+    <Image
+      src="/img/github.svg"
+      alt="GitHub icon"
+      fill={true}
+      style={{ objectFit: 'contain' }}
+    ></Image>
+  ),
+  blog: (
+    <Image
+      src="/img/contact/article.svg"
+      alt="Blog icon"
+      fill={true}
+      style={{ objectFit: 'contain', filter: 'invert(100%)' }}
+    ></Image>
+  ),
+};
