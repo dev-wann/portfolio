511d08d69d11e7588fd31c2762ce32579c341eaf
dev-wann <swcho8220@gmail.com>
2023-09-15 17:29:44 +0900

    [chore] resume 내용 수정 및 페이지 이름 변경
    - resume 내용 수정
    - 페이지 이름을 Seungwan's Portfolio -> Seungwan Cho로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: `Seungwan's Portfolio`,
+  title: `Seungwan Cho`,
   description: 'Portfolio page made by seungwan cho',
 };
 
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -66,25 +66,39 @@ export default function Resume() {
   const contents = (
     <div className={styles.contentWrapper}>
       <div className={styles.content}>
-        <h1 style={{ fontSize: '2.25em' }}>
-          <span style={{ fontSize: '0.9em' }}>Frontend Developer</span>
+        <h1>
+          <span style={{ fontSize: '0.85em' }}>Frontend Developer</span>
           <br />
           조승완입니다.
         </h1>
         <div className={styles.info}>
+          <div style={{ marginBottom: '1rem' }}>
+            <h2 style={{ fontSize: '1.4rem' }}>About</h2>
+            <p style={{ wordBreak: 'keep-all' }}>
+              Web / Electron 기반 문서 편집기를 개발하며 다양한 UI 기능 개발,
+              client side 문서 정보 관리 방안 설계, client side 성능 개선 등의
+              작업을 진행하였습니다. React, TypeScript를 이용한 SPA 개발에
+              익숙하며 MobX를 이용한 상태 관리, Jest를 이용한 테스트 작성 경험이
+              있습니다.
+            </p>
+          </div>
+        </div>
+        <div className={styles.info} style={{ paddingBottom: '0.4rem' }}>
           <div>
-            <h2>About</h2>
+            <h2 style={{ fontSize: '1.4em' }}>Interest</h2>
             <p>직관적으로 이해하기 쉽고 효율적이며</p>
             <p>자연스럽게 사용자의 행동을 유도하는</p>
             <p>UX/UI 개발에 관심이 많습니다.</p>
+            <p>도전을 통해 성장할 기회를 찾고 있습니다.</p>
           </div>
-          <div>
-            <h2>Contact</h2>
+          <div style={{ marginRight: '24px' }}>
+            <h2 style={{ fontSize: '1.4em' }}>Contact</h2>
             <p>
               <Link href="mailto:swcho8220@gmail.com">
                 Email: swcho8220@gmail.com
               </Link>
             </p>
+            <p>Phone: 010-xxxx-xxxx</p>
             <p>
               <Link href="https://github.com/dev-wann">
                 GitHub: https://github.com/dev-wann
@@ -104,7 +118,7 @@ export default function Resume() {
           <div className={styles.row}>
             <div className={styles.row_left}>
               <h2>TmaxOffice</h2>
-              <p>2020.08 ~ 2023.05</p>
+              <p>2020.08 ~ 2023.06</p>
               <p>연구원 / PK본부</p>
             </div>
             <div className={styles.row_right}>
@@ -123,24 +137,29 @@ export default function Resume() {
                   SuperWord(Word processor) 및 SuperPoint(Presentation) 앱의 표
                   공통 기능 개발 담당
                 </li>
+                <li>MVC 패턴의 client-side 문서 정보 관리 모델 설계 및 개발</li>
                 <li>
-                  문서 정보 관리 모델 및 20개 이상의 문서 편집 UI 기능 개발
+                  20개 이상의 문서 편집 UI 기능을 개발하며 event handling,
+                  command handling 로직 구현
                 </li>
-                <li>실시간 다중 사용자 편집 기능 개발</li>
-                <li>JS 로직 및 MobX 최적화를 통한 렌더링 성능 개선 (~70%)</li>
+                <li>
+                  Event loop를 활용한 JS 로직 개선 및 MobX observable 최적화를
+                  통한 clinet-side 문서 편집 성능 개선 (~70%)
+                </li>
                 <li>
                   문서 pagination을 위한 Blink custom layout algorithm 개발
                 </li>
+                <li>실시간 다중 사용자 편집 기능 개발</li>
               </ul>
               <h3>Tech Stack</h3>
               <p>React, TypeScript, MobX, Jest, C++(Blink)</p>
             </div>
           </div>
-          <hr />
+          <hr style={{ margin: '20px 0' }} />
           <div className={styles.row}>
             <div className={styles.row_left}>
               <h2>TmaxA&C</h2>
-              <p>2019.08 ~ 2020.07</p>
+              <p>2019.08 ~ 2020.08</p>
               <p>연구원 / PK본부</p>
             </div>
             <div className={styles.row_right}>
@@ -152,7 +171,7 @@ export default function Resume() {
                 확장자의 문서 호환을 제공합니다.
               </p>
               <p>특허청 및 한국교육학술정보원에 도입되었습니다.</p>
-              <h3>Responsibilities</h3>
+              <h3 style={{ pageBreakBefore: 'always' }}>Responsibilities</h3>
               <ul>
                 <li>
                   ToWord(Word processor), ToPoint(Presentation)의 표 기능 및
@@ -163,12 +182,10 @@ export default function Resume() {
                   개발
                 </li>
                 <li>
-                  Layout algorithm 로직 개선을 통한 프로그램 성능 개선 (~30%)
+                  State 관리를 통한 layout algorithm 로직 개선을 통한 프로그램
+                  레이아웃 성능 개선 (~30%)
                 </li>
-                <li>
-                  다양한 문서 확장자의 parser / writer 구현 (doc, docx, doch,
-                  hwp)
-                </li>
+                <li>doc, docx, doch, hwp 확장자의 parser, writer 구현</li>
                 <li>Legacy code refactoring</li>
               </ul>
               <h3>Tech Stack</h3>
@@ -201,15 +218,18 @@ export default function Resume() {
           <h2>Overall</h2>
           <p>
             프론트엔드는 사용자 경험에 가장 큰 영향을 주는 파트라고 생각합니다.
-            <br />
-            그만큼 사용자를 효과적으로 끌어들일 수 있으면서도 가장 먼저 불편함을
-            초래해 떠나가게도 할 수 있습니다.
-            <br />
-            그렇기 때문에 기능 개발 시 이 기능을 사용자가 논리적으로 납득할 수
-            있을지 항상 고민하고, 좋은 사용성을 제공할 수 있도록 노력합니다.
+          </p>
+          <p>
+            사용자를 효과적으로 끌어들일 수 있으면서도, 사소한 불편함이 가랑비에
+            옷 젖는 줄 모르듯이 쌓여가다 결국 떠나가게도 할 수도 있습니다.
+          </p>
+          <p>
+            그렇기에 기능 개발 시 이 기능을 사용자가 논리적으로 납득할 수 있을지
+            항상 고민하고, 작은 부분에서부터 좋은 사용성을 제공할 수 있도록
+            노력합니다.
           </p>
         </div>
-        <hr style={{ marginLeft: '1em', marginRight: '1em' }} />
+        <hr style={{ margin: '20px 0' }} />
         <div className={styles.desc_other}>
           <h2>Web Development</h2>
           <h3>React</h3>
@@ -226,7 +246,7 @@ export default function Resume() {
             </li>
           </ul>
         </div>
-        <hr style={{ marginLeft: '1em', marginRight: '1em' }} />
+        <hr style={{ margin: '20px 0' }} />
         <div className={styles.desc_other}>
           <h2>Others</h2>
           <h3>협업</h3>
@@ -242,8 +262,8 @@ export default function Resume() {
           <h3>Electron</h3>
           <ul>
             <li>
-              Electron 환경에서의 앱 개발 및 rendering engine 튜닝 경험이
-              있습니다.
+              Electron 환경에서의 앱 개발 및 Chromium rendering engine인 Blink의
+              custom layout algorithm 개발과 디버깅 경험이 있습니다.
             </li>
           </ul>
           <h3>C++</h3>
